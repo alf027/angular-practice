7699e5bfe1560a1cc1d40e6f9fc1bf244f0c00fb
John Alfano <alf027@gmail.com>
2015-08-17 16:00:16 -0600

    her

--- reddit clone/public/javascripts/app.js
+++ reddit clone/public/javascripts/app.js
@@ -1,14 +1,16 @@
-var app = angular.module("redditApp", []);
-
+var app = angular.module("redditApp", ['angularMoment','ngAnimate']);
+//app.run(function(amMoment) {
+//  amMoment.changeLocale('usa');
+//});
 app.controller('RedditController' ,function($scope) {
-
-
-
-  //$scope.showme=false;
   $scope.test = 'test';
-  $scope.posts = [{votes:3, title:'Blah', author:'Alfredicus', imgUrl:'http://placehold.it/350x150',description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, asperiores autem dolores eius enim error facilis, molestiae, molestias perferendis possimus quas quasi qui sint sunt suscipit tempora veniam? Assumenda, optio!"},
-    {votes: 5, title:'Test Title', author:'Alf', imgUrl:'http://placehold.it/350x150',description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, asperiores autem dolores eius enim error facilis, molestiae, molestias perferendis possimus quas quasi qui sint sunt suscipit tempora veniam? Assumenda, optio!"}
-  ]
+  $scope.posts = [{comments:[{author:'Alf',comment:'cool!'}],votes:3, title:'Blah', author:'Alfredicus', imgUrl:'http://www.pageresource.com/wallpapers/wallpaper/cats-cute-cat-animal-cute-grass-photo.jpg',description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, asperiores autem dolores eius enim error facilis, molestiae, molestias perferendis possimus quas quasi qui sint sunt suscipit tempora veniam? Assumenda, optio!"},
+    {comments:[{author:'test',comment:'cool!'},{author:'alfred',comment:'rad!'}],votes: 5, title:'Test Title', author:'Alf', imgUrl:'http://placehold.it/350x150',description:"Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab, asperiores autem dolores eius enim error facilis, molestiae, molestias perferendis possimus quas quasi qui sint sunt suscipit tempora veniam? Assumenda, optio!"}
+  ];
+
+  $scope.posts[0].date = moment([2015, 07, 13]);
+  $scope.posts[1].date = moment([2015, 07, 14]);
+
 
   var Post = function(title,author,imgUrl,description) {
     this.title=title;
@@ -16,16 +18,17 @@ app.controller('RedditController' ,function($scope) {
     this.imgUrl = imgUrl;
     this.description = description;
     this.votes=0;
-    this.date= new Date()
+    this.date= moment();
+    this.comments=[];
 
   };
   $scope.upVote = function (post) {
-    post.votes ++
+    post.votes ++;
     console.log(moment([2015, 07, 13]).calendar());
   };
 
   $scope.downVote = function(post) {
-    post.votes --
+    post.votes --;
   };
 
   $scope.addPost = function (title, author, imgUrl, description, $event) {
@@ -34,5 +37,21 @@ app.controller('RedditController' ,function($scope) {
     $scope.showme=false;
 
     $scope.posts.push(post);
+  }
+
+  //$scope.showCom = function(post){
+  //  if( $scope.showComments === true) {
+  //    $scope.showComments = false;
+  //  } else {
+  //    $scope.showComments = true;
+  //  }
+  //
+  //}
+
+  $scope.addComment = function(author,comment,post) {
+    var commentObj = {};
+    commentObj.author = author;
+    commentObj.comment = comment;
+    post.comments.push(commentObj)
   }
 })
